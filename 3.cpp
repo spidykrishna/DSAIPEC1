e099ef2ef8479534d60d26236b519e9b7ad9a115
spidykrishna <krishnawork69@gmail.com>
2026-02-12 12:11:16 +0530

    Two Sum

---
+++ 3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;  // value -> index
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            
+            if (seen.find(complement) != seen.end()) {
+                return {seen[complement], i};
+            }
+            
+            seen[nums[i]] = i;
+        }
+        
+        return {}; // problem guarantees one solution
+    }
+};
